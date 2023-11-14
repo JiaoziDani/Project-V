8ac72a0d349ec7bea6bc3dc65ce369512ddf1722
BryceJarboe <126623463+BryceJarboe@users.noreply.github.com>
2023-11-14 07:54:06 -0800

    Basic Movement
    WASD Input -> Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,29 +14,44 @@ public class PlayerMovement : MonoBehaviour
 
     Vector3 moveDirection;
 
-    Rigidbody rb;
+    private Rigidbody rb;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        rb.GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
     }
 
+
     // Update is called once per frame
-    void Update()
+    private void Update()
+    {
+        MyInput();
+    }
+    
+
+    void MyInput()
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
     }
 
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+
     private void MovePlayer()
     {
-        /*  Calculate movement direction
-         *  
-         * 
-         */
+        
+        //  Calculate movement direction by scaling axes by corresponding input
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        //  Move the player's rigidbody via force in the calculated direction
+        rb.AddForce(10f * moveSpeed * moveDirection.normalized, ForceMode.Force);
     }
 }
