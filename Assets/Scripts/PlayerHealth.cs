76e5c3d12965f3f3daa3d3f3c3646ee6d8c865a6
rachelrome <rromero8176@sdsu.edu>
2023-11-30 19:17:07 -0800

    Player Death Reloads Scene

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -30,6 +31,6 @@ public class PlayerHealth : MonoBehaviour
 
     private void Die()
     {
-        Destroy(gameObject);
+        SceneManager.LoadScene("SampleScene");
     }
 }
